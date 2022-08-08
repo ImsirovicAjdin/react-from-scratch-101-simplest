a66124166d4767590b6b059769375897c119437b
FlowOverstack <ajdika@live.com>
2022-08-08 13:40:32 +0000

    Complete the reading lesson items code examples

--- src/App.js
+++ src/App.js
@@ -1,7 +1,33 @@
 function App() {
+
+  function thirdExample() {
+    console.log('third example')
+  }
+
+  const fourthExample = () => console.log('fourth example')
+
   return (
     <div className="App">
-      Hello World!
+      <div className="firstExample">
+        <button onClick={function() {console.log('first example')}}>
+          inline anonymous ES5 function event handler
+        </button>
+      </div>
+      <div className="secondExample">
+        <button onClick={() => console.log('second example')}>
+          inline anonymous ES6 function event handler
+        </button>
+      </div>
+      <div className="thirdExample">
+        <button onClick={thirdExample}>
+          using a separate function declaration
+        </button>
+      </div>
+      <div className="fourthExample"> 
+            <button onClick={fourthExample}> 
+                using a separate function expression 
+            </button> 
+        </div>
     </div>
   );
 }
