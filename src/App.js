d925e16668174666d86ba542af1a674bb156a0f4
FlowOverstack <ajdika@live.com>
2022-08-08 09:59:36 +0000

    Add code for the expressions as props reading

--- src/App.js
+++ src/App.js
@@ -1,9 +1,37 @@
-function App() {
+// lesson SharePoint:
+// https://sureskillshq.sharepoint.com/:w:/s/CourseraMetaProgrammes/EbWxxHGwsQlBgd-TJK7encQB4J8LTc2wX5Zr07nPL-xvMw?e=zgrSOq
+
+const bool = false; 
+const firstVal = 10; 
+const secondVal = 2; 
+
+function Example(props) {
   return (
-    <div className="App">
-      Hello World!
-    </div>
-  );
+    <h2>The value of the toggleBoolean prop is: {props.toggleBoolean.toString()}</h2>
+  )
 }
 
-export default App;
+// first example (commented-out code)
+// export default function App() { 
+// 	return ( 
+// 		<div className="App"> 
+// 			<Example toggleBoolean={!bool} /> 
+// 			<button>{Math.random() > 0.5 ? firstVal * secondVal : firstVal / secondVal}</button> 
+// 		</div> 
+// 	) 
+// }
+
+// second example (extracted the ternary into a separate function)
+function randomCalc() {
+  return Math.random() > 0.5 ? firstVal * secondVal : firstVal / secondVal
+}
+
+
+export default function App() { 
+	return ( 
+		<div className="App"> 
+			<Example toggleBoolean={!bool} /> 
+			<button>{randomCalc()}</button> 
+		</div> 
+	) 
+}
