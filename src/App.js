3546f0de209c280600f5d0d1da11bf88aa37b3ee
FlowOverstack <ajdika@live.com>
2022-08-08 08:58:46 +0000

    Start the code updates on the branch

--- src/App.js
+++ src/App.js
@@ -1,3 +1,10 @@
+/*
+4-1-3-10 Style and function 
+
+Task: create a jsx component and apply to another component by passing as a prop. 
+(Provide instructions as to the intended purpose of the components. Provide solution code) 
+Learner gains practical experience in using jsx to style a functional component 
+*/
 function App() {
   return (
     <div className="App">
