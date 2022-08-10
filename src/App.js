bd983186c6737b1b3425de6bae5f0ba3227b88c4
FlowOverstack <ajdika@live.com>
2022-08-10 08:59:29 +0000

    Complete branch for The Navbar video

--- src/App.js
+++ src/App.js
@@ -1,7 +1,26 @@
+import "./App.css";
+import Homepage from "./Homepage";
+import AboutMe from "./AboutMe";
+import { Routes, Route, Link } from "react-router-dom";
+
 function App() {
   return (
     <div className="App">
-      Hello World!
+      {/* <nav className="nav">
+        <a href="#" className="nav-item">Homepage</a>
+        <a href="#" className="nav-item">About Me</a>
+      </nav>
+      <Homepage />
+      <AboutMe /> */}
+	  
+	  <nav>
+      <Link to="/" className="nav-item">Homepage</Link>
+      <Link to="/about-me" className="nav-item">About Me</Link>
+	  </nav>
+      <Routes>
+        <Route path="/" element={<Homepage />}></Route>
+        <Route path="/about-me" element={<AboutMe />}></Route>
+      </Routes>
     </div>
   );
 }
