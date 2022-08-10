bb36dfe4d89e910fdd4c13d8e9b299a8ebd85904
FlowOverstack <ajdika@live.com>
2022-08-10 10:10:06 +0000

    Complete using embedded assets screencast

--- src/App.js
+++ src/App.js
@@ -1,7 +1,28 @@
+import "./App.css";
+import rooftops from "./assets/images/central-park.jpg";
+
 function App() {
+  const randomImgUrl = "https://picsum.photos/450/265";
   return (
     <div className="App">
-      Hello World!
+      <h1>
+        Task: add three images with some styling.
+      </h1>
+      <img 
+        height={200}
+        src={rooftops}
+        alt="Some rooftops close to Central Park"
+      />
+      <img
+        height={200}
+        src={require("./assets/images/mountain-top.jpg")}
+        alt="Mountain top"
+      />
+      <img
+        height={200}
+        src={randomImgUrl}
+        alt="Random img"
+      />      
     </div>
   );
 }
