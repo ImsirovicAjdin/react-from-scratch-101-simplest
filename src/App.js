5db1a8a69883da20046c25d93f197d97fed344b9
FlowOverstack <ajdika@live.com>
2022-08-11 06:31:42 +0000

    Complete the task branch for 4-1-2-5 ungraded lab

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
+function Heading() {
+  return (
+    <h1></h1>
+  )
+}
+
 function App() {
   return (
     <div className="App">
-      Hello World!
+      This is the starting code for "Your first component" ungraded lab
+      <Heading />
     </div>
   );
 }
