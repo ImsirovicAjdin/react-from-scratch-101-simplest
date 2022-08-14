f22db1f78aec33a9cb2596f610190ae0e4e2676f
FlowOverstack <ajdika@live.com>
2022-08-14 06:51:02 +0000

    Complete the task

--- src/04-dog-pics/DogPics.js
+++ src/04-dog-pics/DogPics.js
@@ -1,10 +1,23 @@
+import React from "react";
+
+const getDogPic = async () => {
+  const response = await fetch('https://dog.ceo/api/breeds/image/random');
+  const dog = await response.json();
+  return dog.message;
+}
+
 export default function DogPics () {
-    // API: https://dog.ceo/dog-api/
-    return (
-      <div className='dog-pics'>
-        <img src='https://images.dog.ceo/breeds/spaniel-cocker/n02102318_4172.jpg' />
-        <button>🐶</button>
-      </div>
-    )
-  }
-  
+  // API: https://dog.ceo/dog-api/
+  const [dogPic, setDogPic] = React.useState('')
+  React.useEffect(() => {
+    getDogPic().then(pic => setDogPic(pic))
+  }, []);
+
+  return (
+    <div className='dog-pics'>
+      {/* <img src='https://images.dog.ceo/breeds/spaniel-cocker/n02102318_4172.jpg' /> */}
+      <img src={dogPic} alt="dog pic" />
+      <button onClick={async e => setDogPic(await getDogPic())}>🐶</button>
+    </div>
+  )
+}
