28dfb00c8ed02bb384522de05c68a70e23b7a626
FlowOverstack <ajdika@live.com>
2022-08-15 04:46:22 +0000

    Add 01-08 beginning

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
-function App() {
+import './App.css'
+import PixelArt from './08-pixel-art/PixelArt'
+
+function App () {
   return (
-    <div className="App">
-      Hello World!
+    <div className='App'>
+      <PixelArt />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
