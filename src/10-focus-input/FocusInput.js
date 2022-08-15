75cfff8ed193b4572c35e029ffbeff240bb72529
FlowOverstack <ajdika@live.com>
2022-08-15 07:11:44 +0000

    01-10 focus input, ending

--- src/10-focus-input/FocusInput.js
+++ src/10-focus-input/FocusInput.js
@@ -1,8 +1,15 @@
+import { useEffect } from 'react'
+import {useRef} from 'react'
+
 export default function FocusInput() {
+    const focusedInput = useRef(null)
+    useEffect( () => {
+        focusedInput.current.focus()
+    }, [])
     return (
         <div>
             <label htmlFor='focused-input'>Focus me on page load!</label>
-            <input name='focused-input'></input>
+            <input name='focused-input' ref={focusedInput}></input>
         </div>
     )
 }
